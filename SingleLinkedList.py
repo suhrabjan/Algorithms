9368a2dccd0f7a85300224314c7e56c52e1f8b06
Suhrab Kurbanov <sir.suhrab@gmail.com>
2018-09-16 21:20:40 -0500

    Starting class SingleLinkedList with constructor init and insert_in_beginning method

--- SingleLinkedList.py
+++ SingleLinkedList.py
@@ -1,5 +1,20 @@
 class Node:
     # Class Node that represents a nodes of a singlelinkedlists.
     def __init__(self, value):
-        self.info = value # Actual data that is stored in current node
-        self.link = None # Pointer / reference to the next node
+        self.info = value  # Actual data that is stored in current node
+        self.link = None  # Pointer / reference to the next node
+
+
+class SingleLinkedList:
+
+    def __init__(self):
+        self.start = None
+
+    def insert_in_beginning(self, data):
+        temp = Node(data)
+        temp.link = self.start
+        self.start = temp
+
+
+# l = SingleLinkedList()
+# print(l.start)
