4ed836a1a6b881fd99cb01e4d13c80fc2a5db1bb
Suhrab Kurbanov <sir.suhrab@gmail.com>
2018-09-16 22:14:13 -0500

    adds display_list() method

--- SingleLinkedList.py
+++ SingleLinkedList.py
@@ -10,11 +10,27 @@ class SingleLinkedList:
     def __init__(self):
         self.start = None
 
+    def display_list(self):
+        if self.start is None:
+            return 'List is empty'
+        else:
+            print("List is :  ")
+            p = self.start
+            while p is not None:
+                print(p.info, ' ', end='')
+                p = p.link
+            print()
+
     def insert_in_beginning(self, data):
         temp = Node(data)
         temp.link = self.start
         self.start = temp
 
 
-# l = SingleLinkedList()
+l = SingleLinkedList()
 # print(l.start)
+# print(l.display_list())
+l.insert_in_beginning(5)
+l.insert_in_beginning(5)
+l.insert_in_beginning(5)
+l.display_list()
